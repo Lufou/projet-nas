364a0cbab11df7b559d459c7c884be42f2228313
whitemerch <95518337+whitemerch@users.noreply.github.com>
2023-01-14 16:43:19 +0100

    Presentable
    Missing vrf on ospf and mbgp and vpn on mbgp dk if i should do it have to see with the team

--- nas.py
+++ nas.py
@@ -1,12 +1,12 @@
 import json
 import requests
+import telnetlib
 import time
 
 #negotiation auto
 #The application GNS3 must be openned
-#augmenter le nbr d'interface en fct du fichier
 
-
+#//////////////Creation of the project//////////////
 # Function that checks the other interface
 def check(links, node1, node2):
     lists=links[node2]
@@ -145,3 +145,244 @@ for router, allinks in links.items():
         #    print(response.text)
         #    print("Failed to create link")
 
+#//////////////Routers' configuration//////////////
+
+# Start the routers
+ports={}
+for router,router_id in node_ids.items():
+    response = requests.post(f"{address}/v2/projects/{project_id}/nodes/{router_id}/start", headers=headers)
+    ports[router]=(json.loads(response.text))["console"]
+print("Nodes activated")
+
+# The telnet session for each router configuration
+ip="127.0.0.1"
+for router, port in ports.items():
+    # Create a new Telnet connection
+    tn = telnetlib.Telnet(ip, port)
+    print("created")
+
+    # Initialization
+    tn.write(b"no\r")
+    tn.write(b"\r")
+    tn.write(b"enable\r")  
+
+    # Change the hostname
+    tn.write(b"conf t\r")
+    tn.write(f"hostname {router}\r".encode())
+    tn.write(b"end\r")
+
+    # Check if ipv6
+    #ipv6_present = False
+    #for interface in data[router]['interfaces']:
+    #    if interface['InterfaceName']!="Loopback0":
+    #        if "IPv6" in interface:
+    #            ipv6_present = True
+    #            break
+
+    # Activate ipv6
+    #if ipv6_present:  
+    #    tn.write(b"end\r")
+    #    tn.write(b"conf t\r")
+    #    tn.write(b"ipv6 unicast-routing\r")
+    #    tn.write(b"end\r")
+    
+    # Assign different ip address
+    for interface in data[router]['interfaces']:
+        interface_name=interface["InterfaceName"]
+
+        # Configure ipv4 address
+        if "IPv4" in interface:
+            ipv4=interface["IPv4"]
+            tn.write(b"end\r")
+            tn.write(b"conf t\r")
+
+            # Configuration in an interface
+            tn.write(f"int {interface_name}\r".encode())
+
+            # Configure the address
+            tn.write(f"ip add {ipv4[0]} {ipv4[1]}\r".encode())
+
+            # No shutdown
+            tn.write(b"no shutdown\r")
+            tn.write(b"end\r")
+        
+        # Configure ipv6 address
+        #if "IPv6" in interface:
+        #    ipv6=interface["IPv6"]
+        #    tn.write(b"end\r")
+        #    tn.write(b"conf t\r")
+
+            # Configuration in an interface
+        #    tn.write(f"int {interface_name}\r".encode())
+
+            # Configure the address
+        #    tn.write(f"ipv6 add {ipv6}\r".encode())
+
+            # No shutdown
+        #    tn.write(b"no shutdown\r")
+        #    tn.write(b"end\r")
+        
+    # Check if OSPF
+    if "OSPF_id" in data[router]:
+        ospf_id=data[router]["OSPF_id"]
+        tn.write(b"end\r")  
+        tn.write(b"conf t\r")
+
+        # OSPFv2 activation
+        tn.write(b"router ospf 1\r")
+        tn.write(f"router-id {ospf_id}\r".encode())
+
+        # OSPFv3 if ipv6
+        #if ipv6_present:
+            #tn.write(b"end\r")  
+            #tn.write(b"conf t\r")
+            #tn.write(b"ipv6 router ospf 2\r")
+            #tn.write(f"router-id {ospf_id}\r".encode())
+
+    # Activate OSPF on the interfaces
+    for interface in data[router]['interfaces']:
+        if "OSPF_area" in interface:
+            interface_name=interface["InterfaceName"]
+            ospf_area=interface["OSPF_area"]
+            tn.write(b"end\r")  
+            tn.write(b"conf t\r")
+
+            # Configuration in an interface
+            tn.write(f"int {interface_name}\r".encode())
+
+            # Configuration of the ospf area
+            tn.write(f"ip ospf 1 area {ospf_area}\r".encode())
+            
+            # If ipv6 in interface, OSPFv3
+            #if "IPv6" in interface:
+                #tn.write(f"ipv6 ospf 2 area {ospf_area}\r".encode())
+            
+            #tn.write(b"end\r")
+
+    # Check OSPF neighbors for ipv4
+    if "OSPF_neighboripv4" in data[router]:
+        neighbors=data[router]["OSPF_neighboripv4"]
+        tn.write(b"end\r")  
+        tn.write(b"conf t\r")
+        tn.write(b"router ospf 1\r")
+
+        # We set each neighbor
+        for neighbor in neighbors:
+            tn.write(f"network {neighbor[0]} {neighbor[1]} area {neighbor[2]}\r".encode())
+
+    # Check OSPF Neighbors for ipv6
+    #if "OSPF_neighboripv6" in data[router]:
+    #    neighbors=data[router]["OSPF_neighboripv6"]
+    #    tn.write(b"end\r")  
+    #    tn.write(b"conf t\r")
+    #    tn.write(b"ipv6 router ospf 2\r")
+
+        # We set each neighbor
+    #    for neighbor in neighbors:
+    #        tn.write(f"network {neighbor[0]} {neighbor[1]} area {neighbor[2]}\r".encode())
+    
+
+    # Check if MBBGP
+    if "BGP" in data[router]:
+        ass=data[router]["BGP"]["AS"]
+        tn.write(b"end\r")  
+        tn.write(b"conf t\r")
+        tn.write(f"router bgp {ass}\r".encode())
+        tn.write(b"no bgp default ipv4-unicast\r")
+
+        # Check if MBGP neighbors
+        if "neighbors" in data[router]["BGP"]:
+            # Configure MBGP neighbors
+            neighbors=data[router]["BGP"]["neighbors"]
+            for neighbor in neighbors:
+                tn.write(f"neighbor {neighbor['addr']} remote-as {neighbor['AS']}\r".encode())
+                tn.write(f"neighbor {neighbor['addr']} update-source Loopback 0\r".encode())
+        
+        tn.write(b"end\r")  
+
+        # VPN on MBGP
+
+    # Check Express forwarding
+    if "ipcef" in data[router]:
+        tn.write(b"end\r")  
+        tn.write(b"conf t\r")
+        tn.write(b"ip cef\r")
+        tn.write(b"end\r")
+
+    # Check if MPLS
+    for interface in data[router]['interfaces']:
+        if "MPLS" in interface:
+            interface_name=interface["InterfaceName"]
+            tn.write(b"end\r")  
+            tn.write(b"conf t\r")
+
+            # Configuration in an interface
+            tn.write(f"int {interface_name}\r".encode())
+
+            # Configuration of mpls in the interface
+            tn.write(b"mpls ip\r")
+            tn.write(b"end\r")
+    
+    # Check if VRF
+    if "VRF" in data[router]:
+        for vrf in data[router]["VRF"]:
+
+            # Activate VRF
+            vrf_name=vrf["name"]
+            vrf_rd=vrf["rd"]
+            vrf_import=vrf["rt_import"]
+            vrf_export=vrf["rt_export"]
+
+            tn.write(b"end\r")  
+            tn.write(b"conf t\r")
+            tn.write(f"ip vrf {vrf_name}\r".encode())
+
+            # Configure Route Distinguisher
+            tn.write(f"rd {vrf_rd}\r".encode())
+
+            # Configure Route Target in and out
+            for rt_in in vrf_import:
+                tn.write(f"route-target import {rt_in}\r".encode())
+            for rt_out in vrf_export:
+                tn.write(f"route-target export {rt_out}\r".encode())
+            
+            tn.write(b"end\r")
+
+
+            # Activate VRF on OSPF
+
+            # Activate VRF on BGP
+
+        # Activate VRF on the interfaces
+        for interface in data[router]['interfaces']:
+            if "VRF" in interface:
+                interface_name=interface["InterfaceName"]
+                vrf_name=interface["VRF"]
+                tn.write(b"end\r")  
+                tn.write(b"conf t\r")
+                
+                # Configuration in an interface
+                tn.write(f"int {interface_name}\r".encode())
+
+                # Enable VRF
+                tn.write(f"ip vrf forwarding {vrf_name}\r".encode())
+
+                tn.write(b"end")
+
+    # Save configuration
+    tn.write(b"end\r")
+    tn.write(b"write")
+    tn.write(b"\r")
+
+    print("Ending")
+    time.sleep(5)
+
+
+
+
+
+
+
+
+    
+    
